5a31a302ac1daa97beff7c923f453af02119908f
ngonzalez-ic <44441011+ngonzalez-ic@users.noreply.github.com>
2019-06-04 17:52:40 -0300

    crud php taller 5

---
+++ starter-template/persona.php
@@ -0,0 +1,35 @@
+<?php
+
+class Persona 
+{
+	//atributos
+
+	public $correo;
+	public $clave;
+	public $nombre;
+	public $apellido;
+
+	function guardar()
+	{
+		$archivo=fopen("usuarios.txt", "a");
+
+      $renglon=$this->correo."=>".$this->clave."=>".$this->nombre."=>".$this->apellido."\n";
+
+		fwrite($archivo,$renglon);
+
+		fclose($archivo);
+
+	}
+
+	function mostrar($algo)
+	{
+		echo "hola".$algo;
+
+	}
+
+	
+
+
+}
+
+?>
---
+++ starter-template/verificarLogin.php
@@ -0,0 +1,60 @@
+<?php
+
+session_start();
+function debug($var){
+echo "<pre style='background-color:green; padding:8px; font-size:14px;'>";
+var_dump($var);
+echo "</pre>";
+}
+
+
+/*
+
+
+$archivo=fopen("usuarios.txt", "a");
+$renglon=$_POST["correo"]."=>".$_POST["contraseña"]."\n";
+*/
+$archivo=fopen("usuarios.txt", "r");
+while(!feof($archivo))
+{
+  $renglon = fgets($archivo);
+  //echo $renglon."<br>";
+  $datosArray=explode("=>", $renglon);
+
+
+  debug(array(
+
+   $_POST,
+   ($_POST["correo"] ==trim($datosArray[0])),
+   $datosArray[0]
+
+));
+ 	 //echo "<a href='#' class='list-group-item'>$datosArray[0]</a> <br>";
+ 	 if($_POST["correo"] ==trim($datosArray[0]))
+ 	 {
+ 	 	echo " <br> ok correo $datosArray[1] ". $_POST['contraseña'];
+
+
+      if($_POST["contraseña"]==trim($datosArray[1]))
+ 	 	{
+ 	 		 $_SESSION['usuario']=$_POST["correo"];
+ 	 		echo "<br>  ok clave";
+           //var_dump($_SESSION);
+           header("Location:listarSesion.php");
+        }
+        else{
+         header("Location:login.html");
+        }      
+     }
+     else{
+      header("Location:login.html");
+     }
+}
+echo "sali";
+
+//var_dump($_SESSION);
+
+fclose($archivo);
+
+
+?>
